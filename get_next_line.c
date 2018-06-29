f2dabd9c0dc36a010d49d3613cd5211642f5e9c1
Danyil ZBOROVKYI <dzborovk@e2r5p10.unit.ua>
2018-06-29 21:27:19 +0300

    29.06.2018

--- get_next_line.c
+++ get_next_line.c
@@ -11,9 +11,54 @@
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define BUFF_SIZE 2
+
+int     get_next_line(const int fd, char **line)
+{
+    int     rd;
+    char    buf[BUFF_SIZE + 1];
+
+    rd = 0;
+    while (rd = read(fd, buf, BUFF_SIZE))
+    {
+        buf[rd] = '\0';
+    }
+
+    return ();
+}
 
 int     main(int ac, char **av)
 {
-    ft_putendl(av[1]);
+    int     fd;
+    char    buf[10];
+
+    fd = open("42", O_RDONLY);
+    if (fd == -1)
+    {
+        ft_putendl("Open failed (-1)");
+        return (-1);
+    }
+    get_next_line(fd, buf);
+
+/*   while (rd = read(fd, buf, BUFF_SIZE))
+    {
+        buf[rd] = '\0';
+        ft_putnbr(rd);
+        write(1, "\n", 1);
+        ft_putstr(buf);
+    }
+*/
+
+
+
+    if (close(fd) == -1)
+    {
+        ft_putendl("Close failed (-1)");
+        return (1);
+    }
     return (0);
 }
